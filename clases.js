5d5232b79ace2f482105b858c29147da08379265
Joaska1478 <joaco1478xd@gmail.com>
2024-06-24 15:24:13 -0300

    Update clases.js

--- clases.js
+++ clases.js
@@ -1,1 +1,22 @@
+class Sistema {
+    constructor(preguntas, temas) {
+      this.preguntas = preguntas;
+      this.temas = temas;
+    }
+}
 
+class tema {
+    constructor(nombre, descripcion) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+    }
+}
+
+class Pregunta {
+    constructor(texto, respuestaCorrecta, respuestasIncorrectas, nivel, tema) {
+        this.texto = texto;
+        this.respuestaCorrecta = respuestaCorrecta;
+        this.respuestasIncorrectas = respuestasIncorrectas;
+        this.nivel = nivel;
+        this.tema = tema;
+    }
